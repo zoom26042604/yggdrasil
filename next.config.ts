d710440e1987435e54b0d9badbfa69e62283cd4b
baromain <romain.barreau31@gmail.com>
2025-10-11 18:50:49 +0200

    fix : add auth routes for github and credentials login

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                port: '',
+                pathname: '/u/**',
+            },
+        ],
+    },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+// Remember you must use an AuthProvider for 
+// client components to useSession
+import { useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
+
+
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-2xl font-bold">Test</h1>
+      <h1 className="text-2xl font-bold text-red-500">Test</h1>
     </main>
   );
 }
